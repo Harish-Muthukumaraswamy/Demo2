fbf092efb643cfdae7571eba9a0edb0b5c70ed9f
Harish-Muthukumaraswamy <harish.m1294@gmail.com>
2020-04-24 14:12:25 +0530

    desktopm commit

--- src/test/java/com/example/tests/GitTEst.java
+++ src/test/java/com/example/tests/GitTEst.java
@@ -5,7 +5,12 @@ public class GitTEst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Git TEst");
+System.out.println("Git TEst1");
+System.out.println("Git TEst2");
+System.out.println("Git TEst3");
+System.out.println("Git TEst4");
+
+
 	}
 
 }
